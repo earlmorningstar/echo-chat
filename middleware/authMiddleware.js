a1ea036af4b81ba1804bbfbcda6af5f3e0634334
Onyeabor Joel <earlmorningstar@gmail.com>
2024-12-08 12:33:52 +0100

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const authenticateUser = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1];
+  let token = req.headers.authorization?.split(" ")[1];
   if (!token) {
     return res.status(401).json({ message: "Unauthorized access" });
   }
